c59569eeaa28497d124b7f315373bc03c98ab08e
valhayot <valeriehayot@gmail.com>
2025-02-02 11:04:51 -0600

    Remove unnecessary file and config option

--- __tests__/functions.test.tsx
+++
@@ -1,8 +0,0 @@
-//app.test.tsx
-global.fetch = jest.fn(async () =>
-  Promise.resolve({
-    ok: true,
-    json: async () => Promise.resolve({ id: 0 }),
-    text: async () => Promise.resolve('text')
-  })
-) as jest.Mock
