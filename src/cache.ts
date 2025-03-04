583de38d266d7267daea399cd901702b0df94918
valhayot <valeriehayot@gmail.com>
2025-03-04 16:19:29 -0600

    Add missing cache class

---
+++ src/cache.ts
@@ -0,0 +1,42 @@
+import fs from 'node:fs/promises'
+import * as path from 'path'
+
+export class Cache {
+  dir: string
+
+  constructor(dir: string) {
+    this.dir = path.resolve(dir)
+    fs.mkdir(this.dir, { recursive: true })
+  }
+
+  public async set(
+    key: string,
+    value: string
+  ): Promise<string | null | undefined> {
+    const key_path: string = path.join(this.dir, key)
+    await fs.writeFile(key_path, value)
+    return Promise.resolve(key_path)
+  }
+
+  public async get(key: string): Promise<string | null | undefined> {
+    const key_path: string = path.join(this.dir, key)
+
+    try {
+      const value: string = (await fs.readFile(key_path)).toString('utf-8')
+      return Promise.resolve(value)
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+
+  public async remove(key: string) {
+    const key_path: string = path.join(this.dir, key)
+
+    try {
+      await fs.rm(key_path)
+    } catch (error) {
+      console.debug(error)
+    }
+  }
+}
