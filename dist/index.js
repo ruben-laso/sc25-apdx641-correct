3b8191a0ef4cc4de46c408c1aed125b0a3f317ab
valhayot <valeriehayot@gmail.com>
2025-03-25 19:56:10 -0500

    Add serde length in registration dict

--- dist/index.js
+++ dist/index.js
@@ -31330,7 +31330,7 @@ function getToken(CLIENT_ID, CLIENT_SECRET) {
 function register_function(access_token, shell_cmd) {
     const serialized_body = execSync(`python -c 'import json; import sys; import globus_compute_sdk;` +
         ` data = globus_compute_sdk.serialize.concretes.DillCode().serialize("${shell_cmd}");` +
-        ` print(json.dumps({"function_name": "ci_shell_cmd", "function_code": data, "meta":` +
+        ` print(json.dumps({"function_name": "ci_shell_cmd", "function_code": f"{len(data)}\\n{data}", "meta":` +
         ` { "python_version":  ".".join(str(v) for v in sys.version_info[0:3]),` +
         ` "sdk_version": globus_compute_sdk.__version__, "serde_identifier": "01"}}))'`, { encoding: 'utf-8' });
     console.log(execSync('python --version', { encoding: 'utf-8' }));
